87bc86225404d021c772bec1fc6bb449006b76a2
Carlos Maniero <carlosmaniero@gmail.com>
2016-02-01 15:10:43 -0200

    Add crawlser

---
+++ crawler.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import pymongo
+import os
+import requests
+import time
+import datetime
+
+conn = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_URL'])
+db = conn.requests
+
+while True:
+    req = requests.get('http://cptm.sp.gov.br/Pages/Home.aspx')
+    db.insert({
+        'status_code': req.status_code,
+        'content': req.content,
+        'datetime': datetime.datetime.now()
+    })
+    time.sleep(30)
