ecfd005308d8412b74b00c058235a0a3d5fdc54c
Carlos Maniero <carlosmaniero@gmail.com>
2016-02-02 17:45:41 -0200

    Hide env variables

--- wsgi.py
+++ wsgi.py
@@ -16,9 +16,6 @@ def application(environ, start_response):
     ctype = 'text/plain'
     if environ['PATH_INFO'] == '/health':
         response_body = "1"
-    elif environ['PATH_INFO'] == '/env':
-        response_body = ['%s: %s' % (key, value) for key, value in sorted(environ.items())]
-        response_body = '\n'.join(response_body)
     else:
         ctype = 'text/html'
         response_body = index.format(
